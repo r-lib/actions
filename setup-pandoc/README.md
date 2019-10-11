0955c0ac521ab9a4e309dcd9e4cf7bc1468d4f89
Jim Hester <james.f.hester@gmail.com>
2019-10-11 15:12:39 -0400

    Add setup-pandoc action

---
+++ setup-pandoc/README.md
@@ -0,0 +1,29 @@
+# setup-pandoc
+
+[![RStudio community](https://img.shields.io/badge/community-github--actions-blue?style=social&logo=rstudio&logoColor=75AADB)](https://community.rstudio.com/new-topic?category=Package%20development&tags=github-actions)
+
+This action sets up pandoc by:
+
+- downloading and caching a version of pandoc by version and adding to PATH
+
+# Usage
+
+See [action.yml](action.yml)
+
+Basic:
+```yaml
+steps:
+- uses: actions/checkout@master
+- uses: r-lib/actions/setup-pandoc@v1
+  with:
+    pandoc-version: '2.7.3' # The R version to download (if necessary) and use.
+- run: echo "# Test" | pandoc -t html
+```
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
+
+# Contributions
+
+Contributions are welcome!  See [Contributor's Guide](docs/contributors.md)
---
+++ setup-pandoc/src/setup-pandoc.ts
@@ -0,0 +1,119 @@
+let tempDirectory = process.env["RUNNER_TEMP"] || "";
+
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import * as tc from "@actions/tool-cache";
+import * as path from "path";
+import * as util from "util";
+
+const IS_WINDOWS = process.platform === "win32";
+const IS_MAC = process.platform === "darwin";
+
+if (!tempDirectory) {
+  let baseLocation;
+  if (IS_WINDOWS) {
+    // On windows use the USERPROFILE env variable
+    baseLocation = process.env["USERPROFILE"] || "C:\\";
+  } else {
+    if (IS_MAC) {
+      baseLocation = "/Users";
+    } else {
+      baseLocation = "/home";
+    }
+  }
+  tempDirectory = path.join(baseLocation, "actions", "temp");
+}
+
+async function run() {
+  try {
+    let pandocVersion = core.getInput("pandoc-version");
+    core.debug(`got pandoc-version ${pandocVersion}`);
+    await getPandoc(pandocVersion);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+export async function getPandoc(version: string) {
+  if (IS_WINDOWS) {
+    installPandocWindows(version);
+  } else if (IS_MAC) {
+    installPandocMac(version);
+  } else {
+    installPandocLinux(version);
+  }
+}
+
+async function installPandocMac(version: string) {
+  const fileName = util.format("pandoc-%s-macOS.pkg", version);
+  const downloadUrl = util.format(
+    "https://github.com/jgm/pandoc/releases/download/%s/%s",
+    version,
+    fileName
+  );
+  let downloadPath: string | null = null;
+
+  downloadPath = await tc.downloadTool(downloadUrl);
+  await io.mv(downloadPath, path.join(tempDirectory, fileName));
+
+  exec.exec("sudo installer", [
+    "-pkg",
+    path.join(tempDirectory, fileName),
+    "-target",
+    "/"
+  ]);
+}
+
+async function installPandocWindows(version: string) {
+  const fileName = util.format("pandoc-%s-windows-x86_64.msi", version);
+  const downloadUrl = util.format(
+    "https://github.com/jgm/pandoc/releases/download/%s/%s",
+    version,
+    fileName
+  );
+  let downloadPath: string | null = null;
+
+  downloadPath = await tc.downloadTool(downloadUrl);
+  await io.mv(downloadPath, path.join(tempDirectory, fileName));
+
+  try {
+    await exec.exec(path.join(tempDirectory, fileName), [
+      "/VERYSILENT",
+      "/SUPPRESSMSGBOXES",
+      "/DIR=C:\\pandoc"
+    ]);
+  } catch (error) {
+    core.debug(error);
+
+    throw `Failed to install pandoc: ${error}`;
+  }
+
+  core.addPath(`C:\\pandoc`);
+}
+
+async function installPandocLinux(version: string) {
+  const fileName = util.format("pandoc-%s-1-amd64.deb", version);
+  const downloadUrl = util.format(
+    "https://github.com/jgm/pandoc/releases/download/%s/%s",
+    version,
+    fileName
+  );
+  let downloadPath: string | null = null;
+
+  downloadPath = await tc.downloadTool(downloadUrl);
+  await io.mv(downloadPath, path.join(tempDirectory, fileName));
+
+  try {
+    await exec.exec("sudo gdebi", [
+      "--non-interactive",
+      path.join(tempDirectory, fileName)
+    ]);
+  } catch (error) {
+    core.debug(error);
+
+    throw `Failed to install pandoc: ${error}`;
+  }
+}
+
+run();
