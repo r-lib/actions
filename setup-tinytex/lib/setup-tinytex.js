b8a9610346f6d9a302f91e88f76f69a7981d213d
Alan Haynes <aghaynes@users.noreply.github.com>
2020-05-15 08:59:16 +0200

    remove tostring

--- setup-tinytex/lib/setup-tinytex.js
+++ setup-tinytex/lib/setup-tinytex.js
@@ -113,8 +113,7 @@ function installTinyTexWindows() {
         
         const fs = require("fs");
         console.log(path.join(tempDirectory, fileName));
-        console.log(toString(path.join(tempDirectory, fileName)));
-        var text = fs.readFileSync(toString(path.join(tempDirectory, fileName)), 'utf8');
+        var text = fs.readFileSync(path.join(tempDirectory, fileName), 'utf8');
         var textWithoutLastLine = text.split("\n").slice(0, -1).join('\n');
         fs.writeFile(path.join(tempDirectory, fileName), textWithoutLastLine);
         
