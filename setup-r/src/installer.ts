2b0d1224df1017096ac9271ef0e3dfc3112b7baa
spoltier <simon.poltier@mirai-solutions.com>
2021-01-14 15:09:58 +0100

    Use gcc instead of gfortran, cask not needed

--- setup-r/src/installer.ts
+++ setup-r/src/installer.ts
@@ -89,7 +89,7 @@ async function acquireR(version: string, rtoolsVersion: string) {
 
 async function acquireFortranMacOS() {
   try {
-    await exec.exec("brew", ["install", "--cask", "gfortran"]);
+    await exec.exec("brew", ["install", "gcc"]);
   } catch (error) {
     core.debug(error);
 
