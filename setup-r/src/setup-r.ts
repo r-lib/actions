5d7b954bb8625301db640f6f5c9b891166723c20
Gábor Csárdi <csardi.gabor@gmail.com>
2022-10-24 13:56:51 +0200

    Warn for setup-r@v1

--- setup-r/src/setup-r.ts
+++ setup-r/src/setup-r.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 async function run() {
   try {
     core.debug(`started action`);
+    console.log("::warning title=r-lib/action/check-r-packages@v1 is deprecated::Please update your workflow to use the '@v2' version. Also look at the examples at https://github.com/r-lib/actions/tree/v2/examples because '@v2' workflows are much simpler than '@v1' workflows.");
     let version = core.getInput("r-version");
     core.debug(`got version ${version}`);
 
