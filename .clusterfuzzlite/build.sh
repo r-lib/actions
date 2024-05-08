e23c696fd3a44e12518824b4564cc63ca7862377
Gábor Csárdi <csardi.gabor@gmail.com>
2024-05-08 09:39:16 +0200

    Dummy fuzzer config
    This is hard to do properly without a fully static build of R.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -6,8 +6,15 @@
 # ./configure
 # make -j$(nproc) all
 
+cd /src/actions/src
+$CC -c fuzzer.c
+ar rcs fuzzer.a fuzzer.o
+
 # build fuzzers
 # e.g.
 # $CXX $CXXFLAGS -std=c++11 -Iinclude \
 #     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
 #     $LIB_FUZZING_ENGINE /path/to/library.a
+
+$CC $CFLAGS fuzzer.c -o $OUT/fuzzer \
+    $LIB_FUZZING_ENGINE /src/actions/src/fuzzer.a
