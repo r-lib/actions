990c02d0f622e02ca78aedd340d39217f93f6d18
Gábor Csárdi <csardi.gabor@gmail.com>
2024-05-08 08:20:31 +0200

    Add .clusterfuzzlite config
    [ci skip]

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eu
+
+# build project
+# e.g.
+# ./autogen.sh
+# ./configure
+# make -j$(nproc) all
+
+# build fuzzers
+# e.g.
+# $CXX $CXXFLAGS -std=c++11 -Iinclude \
+#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
+#     $LIB_FUZZING_ENGINE /path/to/library.a
